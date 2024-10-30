73aca0c105c9700232ae2f63819f4c6d5f130799
welingtonalcantara <welingtonalcantara@gmail.com>
2024-10-29 21:07:00 -0300

    refactor: Cria metodo coverte em TarefaListResponse e modifica service para chamar o metodo converte

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaListResponse.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaListResponse.java
@@ -2,9 +2,12 @@ package dev.wakandaacademy.produdoro.tarefa.application.api;
 
 import dev.wakandaacademy.produdoro.tarefa.domain.StatusAtivacaoTarefa;
 import dev.wakandaacademy.produdoro.tarefa.domain.StatusTarefa;
+import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
 import lombok.Value;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Value
 public class TarefaListResponse {
@@ -14,4 +17,17 @@ public class TarefaListResponse {
     private StatusAtivacaoTarefa statusAtivacao;
     private int contagemPomodoro;
 
+    public static List<TarefaListResponse> converte(List<Tarefa> listaTarefas) {
+        return listaTarefas.stream()
+                .map(TarefaListResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    public TarefaListResponse(Tarefa tarefa) {
+        this.idTarefa = tarefa.getIdTarefa();
+        this.descricao = tarefa.getDescricao();
+        this.status = tarefa.getStatus();
+        this.statusAtivacao = tarefa.getStatusAtivacao();
+        this.contagemPomodoro = tarefa.getContagemPomodoro();
+    }
 }
