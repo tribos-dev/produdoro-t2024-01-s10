fc3959716f76e64da6c2c64f534c4c863c1010b7
Thalita Souza <dev.thalita@gmail.com>
2024-11-04 15:49:03 -0300

    fix:retira metodo nao utliizado

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/infra/TarefaSpringMongoDBRepository.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/infra/TarefaSpringMongoDBRepository.java
@@ -1,6 +1,5 @@
 package dev.wakandaacademy.produdoro.tarefa.infra;
 
-import dev.wakandaacademy.produdoro.tarefa.domain.StatusAtivacaoTarefa;
 import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
@@ -8,5 +7,4 @@ import java.util.UUID;
 
 public interface TarefaSpringMongoDBRepository extends MongoRepository<Tarefa, UUID> {
     Optional<Tarefa> findByIdTarefa(UUID idTarefa);
-	Optional<Tarefa> findAllByStatusAtivacaoAndIdUsuario(StatusAtivacaoTarefa ativa, UUID usuario);
 }
