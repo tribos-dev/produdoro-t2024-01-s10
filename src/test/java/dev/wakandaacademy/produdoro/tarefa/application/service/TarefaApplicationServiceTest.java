e470e10835d20b1f3baefb42f2c2556e42c8c203
Karol-Teixeira <karolaine.stefane17@gmail.com>
2024-11-03 22:20:42 -0300

    feat:Implementa Usuario muda Status para pausa Curta

--- src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
@@ -1,13 +1,10 @@
 package dev.wakandaacademy.produdoro.tarefa.application.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
@@ -50,4 +47,17 @@ class TarefaApplicationServiceTest {
         TarefaRequest request = new TarefaRequest("tarefa 1", UUID.randomUUID(), null, null, 0);
         return request;
     }
+
+//    @Test
+//    void deveModificarOrdemTarefa() {
+//        Usuario usuario = DataHelper.createUsuario();
+//        Tarefa tarefa = DataHelper.createTarefa();
+//        NovaPosicaoRequest novaPosicaoRequest = getNovaPosicaoRequest();
+//        when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+//        when(tarefaRepository.buscaTarefaPorId(any())).thenReturn(Optional.of(tarefa));
+//
+//        tarefaApplicationService.modificaOrdemTarefa(usuario.getEmail(), novaPosicaoRequest, tarefa.getIdTarefa());
+//
+//        verify(tarefaRepository, times(1)).salva(tarefa);
+//    }
 }
